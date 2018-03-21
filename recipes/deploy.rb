dd46bca11ee56e8ca83ea22d601dbb86e2430cc1
Usman <usman.khaliq91@gmail.com>
2018-03-21 11:09:38 +0000

    changes

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -8,12 +8,12 @@ rds_db_instance = search("aws_opsworks_rds_db_instance").first
 Chef::Log.info("********** The RDS instance's address is '#{rds_db_instance['address']}' **********")
 Chef::Log.info("********** The RDS instance's password is '#{rds_db_instance['db_password']}' **********")
 Chef::Log.info("********** The RDS instance's user is '#{rds_db_instance['db_user']}' **********")
-end
+
 
 apps.each do |app|
 
   supervisor_service "firebolt" do
-    command "java -Daws.accessKeyId=#{node[:firebolt][:aws_access_key_id]} -Daws.secretKey=#{node[:firebolt][:aws_secret_access_key]} -Dlogging.path=/home/#{node[:firebolt][:user]}/logs -Dsentry.dsn=#{node[:firebolt][:sentry_dsn]} -Dkannel.url=#{node[:firebolt][:kannel_url]} -Dkannel.port=#{node[:firebolt][:kannel_port]} -Dkannel.username=#{node[:firebolt][:kannel_username]} -Dkannel.password=#{node[:firebolt][:kannel_password]}-DAfricell=#{node[:firebolt][:africell_smsCentreNumber]} -DAirtel=#{node[:firebolt][:airtel_smsCentreNumber]} -Dadmin.username=#{node[:firebolt][:admin_username]} -Dadmin.password=#{node[:firebolt][:admin_password]} -jar firebolt.jar"
+    command "java -Daws.accessKeyId=#{node[:firebolt][:aws_access_key_id]} -Daws.secretKey=#{node[:firebolt][:aws_secret_access_key]} -Dlogging.path=/home/#{node[:firebolt][:user]}/logs -Dsentry.dsn=#{node[:firebolt][:sentry_dsn]} -Dkannel.url=#{node[:firebolt][:kannel_url]} -Dkannel.port=#{node[:firebolt][:kannel_port]} -Dkannel.username=#{node[:firebolt][:kannel_username]} -Dkannel.password=#{node[:firebolt][:kannel_password]} d-Dadmin.username=#{node[:firebolt][:admin_username]} -Dadmin.password=#{node[:firebolt][:admin_password]} -Dspring.datasource.url=#{node[:firebolt][:database_url]} -Dspring.datasource.username=#{node[:firebolt][:database_username]} -Dspring.datasource.password=#{node[:firebolt][:database_password]} -jar firebolt.jar"
     directory "/home/#{node[:firebolt][:user]}"
     user node[:firebolt][:user]
     autostart true
@@ -39,8 +39,6 @@ apps.each do |app|
      chmod 755 /home/#{node[:firebolt][:user]}/firebolt.jar;
     EOH
   end
-
-
 
   supervisor_service "firebolt" do
     action :start
